27730ed21407662a574b565ba8b3007f53859039
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-12-06 21:16:25 -0300

    feat: implementado requisito que lista somente a coluna job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,12 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    dataFromCsv = read(path)
+    set_job_type = set()
+    for data in dataFromCsv:
+        set_job_type.add(data["job_type"])
+    orderList = list(set_job_type)
+    return orderList
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
