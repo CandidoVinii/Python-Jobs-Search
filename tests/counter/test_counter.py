77a877f3231eac70628f434247112de084361803
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-12-12 20:16:43 -0300

    feat: implementado requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "tests/mocks/jobs.csv"
+    assert count_ocurrences(path, "full time") == 2
+    assert count_ocurrences(path, "full TIME") == 2
+    assert count_ocurrences(path, "end") == 3
+    assert count_ocurrences(path, "intern") == 0
